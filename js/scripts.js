623110bdc8602d6be01665603dc4f58a967a905b
Ellie Angell and Maxwell Cady <maxwell@>
2016-11-15 10:37:20 -0800

    add js

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+$(function() {
+  $("#ocelot").click(function() {
+    $("#ocelotOutput").show();
+    $("#ocelotOutput").siblings().hide();
+  })
+  $("#panda").click(function() {
+    $("#pandaOutput").show();
+    $("#pandaOutput").siblings().hide();
+  })
+  $("#wildass").click(function() {
+    $("#wildassOutput").show();
+    $("#wildassOutput").siblings().hide();
+  })
+  $("#humpback").click(function() {
+    $("#humpbackOutput").show();
+    $("#humpbackOutput").siblings().hide();
+  })
+  $("#sealion").click(function() {
+    $("#sealionOutput").show();
+    $("#sealionOutput").siblings().hide();
+  })
+  $("#kangaroo").click(function() {
+    $("#kangarooOutput").show();
+    $("#kangarooOutput").siblings().hide();
+  })
+  $("#argali").click(function() {
+    $("#argaliOutput").show();
+    $("#argaliOutput").siblings().hide();
+  })
+  $("#bison").click(function() {
+    $("#bisonOutput").show();
+    $("#bisonOutput").siblings().hide();
+  })
+  $("#wolf").click(function() {
+    $("#wolfOutput").show();
+    $("#wolfOutput").siblings().hide();
+  })
+  $("#zebra").click(function() {
+    $("#zebraOutput").show();
+    $("#zebraOutput").siblings().hide();
+  })
+  $("#addax").click(function() {
+    $("#addaxOutput").show();
+    $("#addaxOutput").siblings().hide();
+  })
+
+})
